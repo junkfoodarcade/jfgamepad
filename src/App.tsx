f079ea436cd1a74343d6540f902dd6f178921c86
Gary Storey <garystorey655@gmail.com>
2021-03-29 01:02:44 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useGamepad } from './hooks/useGamepad'
 import { ColorSelector, Show } from './components'
 import {SnackboxMicro} from './gamepads'
-
 import './App.css'
 
 const defaultOptions =  { connected: false, pressed:[], length:0 };
@@ -15,12 +14,13 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1><img src="./logo.png" alt="Junkfood Arcade Logo" width="200" style={{float:"left"}}/></h1>
         <main>
       <Show when={!connected}>
         <p>Press a button on the controller to begin</p>
       </Show>
       <Show when={!!connected}>
-        <ColorSelector id="highlight" label="Highlight" initial={color} setValue={setColor} />
+        <ColorSelector id="highlight" label="Button Highlight" initial={color} setValue={setColor} />
         <SnackboxMicro totalButtons={length} pressed={pressed} color={color} />
       </Show>
       </main>
