581b2d4714d1b49fa099ee98d04be93166ead999
Gary Storey <garystorey655@gmail.com>
2021-03-29 01:53:19 -0400

    start button upgrade

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,39 +1,35 @@
 import React, { memo } from 'react'
-import './Button.css';
+import './Button.css'
 
 type ButtonProps = {
   index: number
   className?: string
   pressed?: boolean
   name: string
-  color: string;
+  color: string
+  size: 'sm' | 'med' | 'lg'
 }
 
-export const Button = ({
-  index,
-  className = '',
-  pressed = false,
-  name,
-  color,
-}: ButtonProps) => {
+export const Button = ({ index, className = '', pressed = false, name, color, size = 'med' }: ButtonProps) => {
   const id = `b${index}`
-  const arrows= ['↑','↓','←','→'];
-  let arrow='';
-  if(arrows.includes(name)) {
-    arrow = (name === '↑') ? 'up'
-          : (name === '↓') ? 'down'
-          : (name === '←') ? 'left'
-          : 'right';
+  const arrows = ['↑', '↓', '←', '→']
+  let arrow = ''
+  if (arrows.includes(name)) {
+    arrow = name === '↑' ? 'up' : name === '↓' ? 'down' : name === '←' ? 'left' : 'right'
   }
 
-  const classes = `button${className ? ' ' + className : ''}${pressed ? ' pressed' : ''} ${id.toLowerCase()}`
-   //${arrow? arrow : name.toLowerCase()}
-  const styles = pressed ? {
-    backgroundColor: color
-  } : {};
+  const classes = `button${className ? ' ' + className : ''}${
+    pressed ? ' pressed' : ''
+  } ${id.toLowerCase()} ${`button-${size}`}`
+  //${arrow? arrow : name.toLowerCase()}
+  const styles = pressed
+    ? {
+        backgroundColor: color,
+      }
+    : {}
   return (
     <span className={classes} style={styles}>
-      {name}
+      <span className="inner">{name}</span>
     </span>
   )
 }
