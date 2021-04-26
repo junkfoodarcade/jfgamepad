eb207f2daf28d319016231ebc7433ae59d850df9
Gary Storey <garystorey655@gmail.com>
2021-04-26 01:36:17 -0400

    fix: wrong var name

--- functions/play.js
+++ functions/play.js
@@ -3,7 +3,7 @@ const { query, client } = require('../config/fauna')
 exports.handler = (event) => {
   const d = event.queryStringParameters.d
 
-  client.query(query.Get(q.Ref(q.Collection('entries'), d))).then((res) =>
+  client.query(query.Get(query.Ref(query.Collection('entries'), d))).then((res) =>
     ({
       statusCode: 200,
       res,
