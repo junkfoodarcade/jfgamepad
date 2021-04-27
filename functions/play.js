c655a10b9702156ba20b2fc313732d6eb9e7233d
Gary Storey <garystorey655@gmail.com>
2021-04-26 23:42:08 -0400

    fix: try again

--- functions/play.js
+++ functions/play.js
@@ -1,19 +1,13 @@
 const { q, client } = require('../config/fauna')
 
-exports.handler = (event) => {
+exports.handler = async (event) => {
+  console.log('Start')
   const d = event.queryStringParameters.d
-
-  client
-    .query(q.Map(q.Paginate(q.Match(q.Index('session_ind', d))),
-      q.Lambda('pilotRef', q.Get(q.Var('pilotRef')))))
-    .then((res) => {
-      console.log(JSON.stringify(res))
-      return JSON.stringify(res)
-    })
-    .catch((err) => {
-      return {
-        statusCode: 400,
-        err,
-      }
-    })
+  const res = await client.query(q.Match(q.Index('session_ind', d)))
+  console.log(res)
+  console.log(JSON.stringify(res));
+  return {
+    statusCode: 900,
+    body: res
+  }
 }
