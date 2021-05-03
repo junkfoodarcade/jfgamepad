8b62bc3917bd655610194e016a8f324ef4afc4ab
Gary Storey <garystorey655@gmail.com>
2021-05-03 01:28:59 -0400

    add play

---
+++ functions/play.js
@@ -0,0 +1,15 @@
+const client  = require('../config/db')
+
+exports.handler = async (event) => {
+  const sid = event.queryStringParameters.d
+  await client.connect();
+  const db =client.db('JunkFood');
+  const collection = db.collection('data');
+  const data = collection.find({sid});
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+
+}
