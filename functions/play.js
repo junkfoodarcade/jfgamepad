c9b33ce9668f9e2bc5bdd31762e5648aa53e729f
Gary Storey <garystorey655@gmail.com>
2021-05-03 01:43:50 -0400

    remove es6

--- functions/play.js
+++ functions/play.js
@@ -1,9 +1,9 @@
-import { connect, db } from '../config/db';
-
-export async function handler(event) {
+const client = require('../config/db')
+exports.handler = async (event) => {
   const sessionId = event.queryStringParameters.d
-  await connect();
-  const data = db('JunkFood').collection('data').find({sessionId:sessionId})
+  await client.connect();
+  const data = client.db('JunkFood').collection('data').find({sessionId:sessionId})
+
   return {
     statusCode: 200,
     body: JSON.stringify(data)
