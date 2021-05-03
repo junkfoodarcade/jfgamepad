1f4d7421533471e93b410a095aa129d3c11c278b
Gary Storey <garystorey655@gmail.com>
2021-05-03 01:48:41 -0400

    try hard code

--- functions/play.js
+++ functions/play.js
@@ -2,8 +2,11 @@ const client = require('../config/db')
 exports.handler = async (event) => {
   const sessionId = event.queryStringParameters.d
   await client.connect();
-  const data = client.db('JunkFood').collection('data').find({sessionId:sessionId})
-
+  const data = client.db('JunkFood').getCollection('data')
+  .find(
+    {
+      sessionId: "ea406704-2a8e-4c7f-b1ef-1ab785dcd1e2"
+    })
   return {
     statusCode: 200,
     body: JSON.stringify(data)
