2b44705ae2f4d8227c1d7b4898b535da79f7c2c2
Gary Storey <garystorey655@gmail.com>
2021-05-03 01:54:33 -0400

    again

--- functions/play.js
+++ functions/play.js
@@ -1,15 +1,12 @@
 const client = require('../config/db')
 exports.handler = async (event) => {
   const sessionId = event.queryStringParameters.d
-  await client.connect();
-  const data = client.db('JunkFood').collection('data')
-  .find(
-    {
-      sessionId: "ea406704-2a8e-4c7f-b1ef-1ab785dcd1e2"
-    })
+  await client.connect()
+  const data = client.db('JunkFood').collection('data').find({
+    sessionId: sessionId,
+  })
+  console.log(data);
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
   }
-
 }
