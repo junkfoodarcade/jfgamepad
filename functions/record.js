df2f2762d22b159e5654c5297ad1ecaddbca2f61
Gary Storey <garystorey655@gmail.com>
2021-05-03 01:18:43 -0400

    seeing if this clears memory

--- functions/record.js
+++ functions/record.js
@@ -6,12 +6,20 @@ exports.handler = async (event) => {
   await client.connect();
   const db =client.db('JunkFood');
   const data = db.collection('data');
-  let item = {
+  const item = {
     "_createdAt": +ts,
     "sessionId": sid,
     btn: +btn
   };
   const {insertedId} = await data.insertOne(item);
+
+  d=null;
+  ts=null;btn=null;sid=null;
+  client=null;
+  db =null;
+  data=null;
+  item =null
+
   return {
     statusCode: 200,
     body: JSON.stringify({id:insertedId})
